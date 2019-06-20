68dbc7ad6a058d51e089adf74e63532735b024d5
xuxueyong <1973376397@qq.com>
2019-06-20 14:10:29 +0800

    js调用修改

--- www/yzjmessage.js
+++ www/yzjmessage.js
@@ -9,17 +9,17 @@ exports.getToken = function(success, error,codes) {
    
 // 打开消息列表
 exports.openMessageListView = function(success, error,codes) {
-    exec(success, error, "CDVYZJMessage", "getToken",codes);
+    exec(success, error, "CDVYZJMessage", "openMessageListView",codes);
 };
     
 // 打开通讯录聊天
 exports.openPersonMessageView = function(success, error,codes) {
-    exec(success, error, "CDVYZJMessage", "getToken",codes);
+    exec(success, error, "CDVYZJMessage", "openPersonMessageView",codes);
 };
     
 // 分享
 exports.shareMessageToSDK = function(success, error, userName) {
-    exec(success, error, "CDVYZJMessage", "getExtra" , userName);
+    exec(success, error, "CDVYZJMessage", "shareMessageToSDK" , userName);
 };
 
 exports.allowPermission = function(success, error,codes) {
